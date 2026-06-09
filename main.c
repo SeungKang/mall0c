035ee8a2638cd53cd492a3e961e472edbb9162b5
kang_ <kang_@bull.local>
2026-06-09 16:03:56 -0400

    main: add c compiler macro for each os for size

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef __APPLE__
-#include <malloc/malloc.h>
-#endif
-
 int main()
 {
   setbuf(stdout, 0);
   void *ptr = malloc(0);
   printf("ptr is %p\n", ptr);
 
+  size_t size = 0; 
+
 #ifdef __APPLE__
-  size_t size =  malloc_size(ptr);
-  printf("allocated size is %zu\n", size);
+  #include <malloc/malloc.h>
+  size =  malloc_size(ptr);
+#elif __FreeBSD__
+  #include <malloc_np.h>
+  size = malloc_usable_size(ptr);
+#elif __OpenBSD__
+  #include <malloc_np.h>
+  size = malloc_usable_size(ptr);
+#elif __NetBSD__
+  #include <malloc_np.h>
+  size = malloc_usable_size(ptr);
+#elif __linux__
+  #include <malloc.h>
+  size = malloc_usable_size(ptr);
 #endif
+
+  if (size != 0) {
+    printf("allocated size is %zu\n", size);
+  }
 
   printf("writing to chunk\n");
   ((char *)ptr)[0] = 'A';
